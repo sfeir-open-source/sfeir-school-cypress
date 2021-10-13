bce21166a7cb402c33381a7b830b3ea0e7cfc0d1
Jonathan Geoffroy <geoffroy.jonathan@gmail.com>
2021-10-13 10:03:44 +0200

    initialize react solution

---
+++ exercices/steps/tailwind.config.js
@@ -0,0 +1,13 @@
+// const { createGlobPatternsForDependencies } = require('@nrwl/react/tailwind');
+
+module.exports = {
+  purge: ['./apps/react-solution/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
