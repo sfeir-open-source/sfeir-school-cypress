551427c01f2e84545375f3828e8ed3e3d8a34d4a
Jonathan Geoffroy <geoffroy.jonathan@gmail.com>
2021-11-13 18:55:15 +0100

    chore: exercice02

---
+++ exercices/steps/apps/exercice02-e2e/src/integration/header.spec.ts
@@ -0,0 +1,15 @@
+describe('Header', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('should display Sfeir header',() => {
+     // eslint-disable-next-line no-unused-expressions
+     expect("Learning").to.be.null
+  })
+
+  it('should go to sign-in page when user clicks on button',() => {
+     // eslint-disable-next-line no-unused-expressions
+     expect("Learning").to.be.null
+  })
+})
