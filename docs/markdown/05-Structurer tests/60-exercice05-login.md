79c03a25055ab86cf1a6218f148a154c54a7e1a7
Jonathan Geoffroy <geoffroy.jonathan@gmail.com>
2021-11-15 18:04:42 +0100

    Split exercices 4 and 5

---
+++ docs/markdown/05-Structurer tests/60-exercice05-login.md
@@ -0,0 +1,10 @@
+<!-- .slide: class="center" -->
+
+# Exercice 5 : Login
+
+ * Tester la page login
+   * Un maximum de cas possibles
+
+ * Tester les recommendations (top-10)
+   * déconnecté (placeholder présent)
+   * connecté (top-10 présent)
--- docs/scripts/slides.js
+++ docs/scripts/slides.js
@@ -5,6 +5,7 @@ function school() {
     "00-school/speaker-nfr.md",
   ]
 }
+
 function intro() {
   return [
     "01-intro/00-TITLE.md",
@@ -12,6 +13,7 @@ function intro() {
     "01-intro/30-how.md",
   ]
 }
+
 function cypress() {
   return [
     "02-Cypress/00-TITLE.md",
@@ -23,6 +25,7 @@ function cypress() {
     "02-Cypress/60-exercice01-static.md",
   ]
 }
+
 function goodtest() {
   return [
     "03-Good test/00-TITLE.md",
@@ -32,6 +35,7 @@ function goodtest() {
     "03-Good test/40-exercice02-query.md",
   ]
 }
+
 function networking() {
   return [
     "04-Networking/00-TITLE.md",
@@ -44,13 +48,16 @@ function networking() {
     "04-Networking/60-ouverture.md",
   ]
 }
+
 function structurertests() {
   return [
     "05-Structurer tests/00-TITLE.md",
-    "05-Structurer tests/10-differents-tests.md",
     "05-Structurer tests/20-command.md",
-    "05-Structurer tests/30-exercice04-login.md",
-    "05-Structurer tests/40-ouverture.md",
+    "05-Structurer tests/30-exercice04-commands.md",
+    "05-Structurer tests/40-command-ouverture.md",
+    "05-Structurer tests/50-differents-tests.md",
+    "05-Structurer tests/60-exercice05-login.md",
+    "05-Structurer tests/70-ouverture.md",
   ]
 }
 
